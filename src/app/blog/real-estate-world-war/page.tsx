debb11f535f477abd4f2b3fa061515dfe8d7a889
Swapnil <aurelionluxury@gmail.com>
2026-05-26 14:23:47 +0530

    Add real estate blog post

---
+++ src/app/blog/real-estate-world-war/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <iframe
+      src="/blog-posts/real-estate.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+    />
+  )
+}
